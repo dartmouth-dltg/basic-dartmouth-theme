cca3f85f3540e402aa066e1ba7e1b48026a7645d
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-08-30 18:42:56 -0400

    Slick Carousel
    - add slick carousel (accessible) as dependency
    - move dltg/gulp-build to dev dependencies
    - add initial script and styling for slick

--- asset/js/default.js
+++ asset/js/default.js
@@ -1,1 +1,26 @@
-var Omeka = {};
+(function($) {
+  $(document).ready(function() {
+    $('.item.resource.show .media-embeds').each(function(){
+      if ($(this).children('.media-render').length == 1) {
+        console.log('foo')
+        return;
+      }
+      $(this).slick(
+        {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: false,
+          autoplaySpeed: 8000,
+          dots: true,
+          adaptiveHeight: false,
+          accessibility: true,
+          arrowsPlacement: "split",
+          accessibility: true,
+          focusOnSelect: true,
+          fade: true,
+          cssEase: 'linear',
+        }
+      );
+    });
+  });
+})(jQuery);
