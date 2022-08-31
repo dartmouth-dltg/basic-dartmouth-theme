d6710c1632cf7b90ae3f2020022827158b972ef2
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-08-31 13:38:16 -0400

    Lightbox
    - add lightbox feature
    - additional tweaks

--- asset/js/default.js
+++ asset/js/default.js
@@ -1,5 +1,12 @@
 (function($) {
+
+  function createUniqueId() {
+    return uuidv4();
+  }
+
   $(document).ready(function() {
+
+    // set up accessible slick for multi-image media embeds
     $('.item.resource.show .media-embeds').each(function(){
       if ($(this).children('.media-render').length == 1) {
         return;
@@ -22,9 +29,55 @@
       );
     });
 
+    // enhance values that are plain text links by making them actionable links
     $('.value-content').each(function() {
       const newText = $(this).text().autoLink();
       $(this).html(newText);
     })
+
+    // add unique ids to the images. we'll need this to sync the lightbox slider and slick slider
+    $('.item.resource.show .media-embeds .media-render img').parent('a').each(function() {
+      $(this).addClass('dcl-lightbox');
+      const uuid = createUniqueId;
+      $(this).attr('id', uuid);
+    });
+
+    const tobii = new Tobii({
+      selector: '.dcl-lightbox'
+    });
+
+    $('.tobii-image img').each(function() {
+      const src = $(this).data('src')
+      const slickImgId = $('a[href="' + src + '"]').attr('id')
+      $(this).data('slick-id', slickImgId)
+    });
+
+    $('.tobii-zoom__icon').attr('tabindex',0);
+
+    // basic sync lightbox and carousel
+    $('.tobii__btn--next, .tobii__btn--previous').click(function() {
+      // find active slide
+      const activeSlide = $('.tobii__slide--is-active');
+      const activeId = activeSlide.find('img').data('slick-id');
+      const linkedHref = $('a[id="' + activeId + '"]');
+      const activeSlider = linkedHref.closest('.slick-slider');
+      if ($(this).hasClass('tobii__btn--next')) {
+        activeSlider.slick('slickNext');
+      }
+      else activeSlider.slick('slickPrev');
+    });
+
+    // expand/compress image in lightbox
+    $('a.lightbox-control').click(function(e) {
+      e.preventDefault();
+      if ($(this).children('.fa').hasClass('fa-expand')) {
+        $(this).children('.fa').removeClass('fa-expand').addClass('fa-compress');
+        $(this).closest('.lightbox-details-img').removeClass('with-description');
+      }
+      else {
+        $(this).children('.fa').removeClass('fa-compress').addClass('fa-expand');
+        $(this).closest('.lightbox-details-img').addClass('with-description');
+      }
+    });
   });
 })(jQuery);
